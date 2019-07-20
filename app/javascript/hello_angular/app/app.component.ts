53b9712abc5f2baafb31ea83688239f68a2b8a9b
Saharak <saharak@bananacoding.com>
2019-07-20 17:29:40 +0700

    [Setup]

---
+++ app/javascript/hello_angular/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import templateString from './app.component.html'
+
+@Component({
+  selector: 'hello-angular',
+  template: '<h1>2222</h1>'
+})
+
+export class AppComponent {
+  name = 'Angular!'
+}
