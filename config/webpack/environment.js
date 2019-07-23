07ad91580977abc3ea9a0b407618d02311929deb
Saharak <saharak@bananacoding.com>
2019-07-24 01:14:10 +0700

    [Fixed] loader

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -3,17 +3,8 @@ const typescript = require('./loaders/typescript')
 
 environment.loaders.prepend('typescript', typescript)
 environment.loaders.append('html', {
-  test: /\.(ts|tsx)?(\.erb)?$/,
-  use: [{
-    loader: 'html-loader',
-    options: {
-      minimize: true,
-      removeAttributeQuotes: false,
-      caseSensitive: true,
-      customAttrSurround: [[/#/, /(?:)/], [/\*/, /(?:)/], [/\[?\(?/, /(?:)/]],
-      customAttrAssign: [/\)?\]?=/]
-    }
-  }]
+  test: /\.html$/,
+  use: 'html-loader'
 })
 
 module.exports = environment
