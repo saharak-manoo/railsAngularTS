fe1d5a3e54da185f91f1595e9ae318e43c993335
Saharak <saharak@bananacoding.com>
2019-07-24 01:09:27 +0700

    [Fixed] code

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,17 +1,17 @@
 const { environment } = require('@rails/webpacker')
-const typescript =  require('./loaders/typescript')
+const typescript = require('./loaders/typescript')
 
 environment.loaders.prepend('typescript', typescript)
 environment.loaders.append('html', {
-  test: /\.html$/,
+  test: /\.(ts|tsx)?(\.erb)?$/,
   use: [{
     loader: 'html-loader',
     options: {
       minimize: true,
       removeAttributeQuotes: false,
       caseSensitive: true,
-      customAttrSurround: [ [/#/, /(?:)/], [/\*/, /(?:)/], [/\[?\(?/, /(?:)/] ],
-      customAttrAssign: [ /\)?\]?=/ ]
+      customAttrSurround: [[/#/, /(?:)/], [/\*/, /(?:)/], [/\[?\(?/, /(?:)/]],
+      customAttrAssign: [/\)?\]?=/]
     }
   }]
 })
