334df9acb123401d44b35e94d01424f710d3cb25
Saharak <saharak@bananacoding.com>
2019-07-24 10:24:42 +0700

    [Fixed] code

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -2,6 +2,6 @@ Rails.application.config.content_security_policy do |policy|
   if Rails.env.development?
     policy.script_src :self, :https, :unsafe_eval
   else
-    policy.script_src :self, :https
+    policy.script_src :self, :https, :unsafe_eval
   end
 end
